717efdba0e809773aa4a325d1de3e01aba53176c
akashdeep-star <akash_deep.vadicherla@novartis.com>
2025-02-25 17:15:28 +0530

    added storage browser component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { Authenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import outputs from '../amplify_outputs.json';
 import '@aws-amplify/ui-react/styles.css';
+import { StorageBrowser } from '../components/StorageBrowser';
+
+
 // const client = generateClient<Schema>();
 Amplify.configure(outputs);
 
@@ -21,6 +24,9 @@ function App() {
         <main>
           <h1>Hello {user?.username}</h1>
           <button onClick={signOut}>Sign out</button>
+          {/* StorageBrowser Component */}
+          <h2>Your Files</h2>
+          <StorageBrowser />
         </main>
       )}
     </Authenticator>

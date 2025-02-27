edfb5bf55db7fc6747424ddd7a1e2775fb263e3f
Vadicherla <vadicak1@novartis.net>
2025-02-27 15:56:04 +0530

    configuring kms encryption

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,9 +2,26 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+// import { aws_s3 as s3 } from 'aws-cdk-lib';
 
-defineBackend({
+// defineBackend({
+//   auth,
+//   data,
+//   storage
+// });
+const backend = defineBackend({
   auth,
   data,
   storage
 });
+
+const s3Bucket = backend.storage.resources.bucket;
+
+
+const cfnBucket = s3Bucket.node.defaultChild as s3.CfnBucket;
+
+
+const encryptionConfigurationProperty: s3.CfnBucket.EncryptionConfigurationProperty = {
+  replicaKmsKeyId: 'arn:aws:kms:eu-west-1:082449889088:key/e78b4d9c-e2e1-4e4e-bb64-73943d1601c1',
+};

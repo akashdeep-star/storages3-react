b05680d80d9bc008d48fa4c7b99ac3d54a1aba28
Vadicherla <vadicak1@novartis.net>
2025-02-27 16:21:14 +0530

    adding policy and encryption

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -22,6 +22,38 @@ const s3Bucket = backend.storage.resources.bucket;
 const cfnBucket = s3Bucket.node.defaultChild as s3.CfnBucket;
 
 
-const encryptionConfigurationProperty: s3.CfnBucket.EncryptionConfigurationProperty = {
-  replicaKmsKeyId: 'arn:aws:kms:eu-west-1:082449889088:key/e78b4d9c-e2e1-4e4e-bb64-73943d1601c1',
+// const encryptionConfigurationProperty: s3.CfnBucket.EncryptionConfigurationProperty = {
+//   replicaKmsKeyId: 'arn:aws:kms:eu-west-1:082449889088:key/e78b4d9c-e2e1-4e4e-bb64-73943d1601c1',
+// };
+
+cfnBucket.bucketEncryption = {
+  serverSideEncryptionConfiguration: [
+    {
+      serverSideEncryptionByDefault: {
+        sseAlgorithm: 'aws:kms',
+        kmsMasterKeyId:'arn:aws:kms:eu-west-1:082449889088:key/e78b4d9c-e2e1-4e4e-bb64-73943d1601c1'
+      },
+      bucketKeyEnabled: true 
+    }
+  ]
 };
+
+cfnBucket.addPropertyOverride('BucketPolicy', {
+  PolicyDocument: {
+    Version: "2012-10-17",
+    Statement: [
+      {
+        Sid: "EnforceEncryption",
+        Effect: "Deny",
+        Principal: "*",
+        Action: "s3:PutObject",
+        Resource: `${s3Bucket.bucketArn}/*`,
+        Condition: {
+          StringNotEquals: {
+            "s3:x-amz-server-side-encryption": "aws:kms"
+          }
+        }
+      }
+    ]
+  }
+});

ab4698bb4a2775b0e23aba0631c77ef60d9eaab5
Vadicherla <vadicak1@novartis.net>
2025-03-14 11:31:45 +0530

    endpoint

--- components/StorageBrowser.tsx
+++ components/StorageBrowser.tsx
@@ -8,6 +8,47 @@ import config from '../amplify_outputs.json';
 Amplify.configure(config);
 
 // Create the StorageBrowser component with Amplify authentication
+// export const { StorageBrowser } = createStorageBrowser({
+//   config: createAmplifyAuthAdapter(),
+// });
+
+// interface Config {
+//   accountId?: string;
+//   customEndpoint?: string;
+//   getLocationCredentials: GetLocationCredentials;
+//   listLocations: ListLocations;
+//   registerAuthListener: RegisterAuthListener;
+//   region: string;
+// }
 export const { StorageBrowser } = createStorageBrowser({
-  config: createAmplifyAuthAdapter(),
-});
+	config: {
+		...createAmplifyAuthAdapter(),
+		customEndpoint: '*.vpce-0d645cb58afb1131f-2o7g1hwv.s3.us-east-1.vpce.amazonaws.com'
+	}
+})
+
+// const { StorageBrowser } = createStorageBrowser({
+//   actions: {
+//     default: {
+//       upload: {
+//         ...defaultActionConfigs.upload,
+//         handler: ({ config, data, options }) =>
+//           defaultActionConfigs.upload.handler({
+//             config,
+//             data,
+//             options: {
+//               ...options,
+//               onSuccess: (...input) => {
+//                 options?.onSuccess?.(...input);
+//                 fileUploadCounter.add(1); // Your custom counter metrics.
+//               },
+//               onError: (data, message, error) => {
+//                 options?.onError?.(data, message, error);
+//                 fileUploadError.recordError(error); // Your custom error metrics.
+//               },
+//             },
+//           }),
+//       }
+//     }
+//   }
+// });

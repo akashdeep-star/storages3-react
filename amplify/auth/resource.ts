a46217b15fa165c2cdd85031e3433cb161a17a19
Vadicherla <vadicak1@novartis.net>
2025-03-26 21:33:21 +0530

    endpoint

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -7,5 +7,21 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      saml: {
+        name: 'IAMIdC',
+        metadata: {
+          metadataContent: 'https://portal.sso.us-east-1.amazonaws.com/saml/metadata/NTcxNDA1MzMxMzU2X2lucy1iNmRhOGY3MThkZDBkYWVh', 
+          metadataType: 'URL', // or 'FILE'
+        },
+        attributeMapping: {
+          email: 'email'
+        }
+      },
+      logoutUrls: ['http://localhost:5173/', 'https://main.d7avcxipkzn8r.amplifyapp.com/'],
+      callbackUrls: [
+        'http://localhost:5173/', 'https://main.d7avcxipkzn8r.amplifyapp.com/'
+      ],
+    },
   },
 });

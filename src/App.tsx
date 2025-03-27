7445571d0dfb27fdec55e6a0ff90ec4c9072bd08
Vadicherla <vadicak1@novartis.net>
2025-03-27 14:51:33 +0530

    endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,35 @@ import { Amplify } from 'aws-amplify';
 import outputs from '../amplify_outputs.json';
 import '@aws-amplify/ui-react/styles.css';
 import { StorageBrowser } from '../components/StorageBrowser';
-
+import { getCurrentUser ,signInWithRedirect, signOut } from 'aws-amplify/auth';
+import { useState, useEffect } from "react";
 
 // const client = generateClient<Schema>();
 Amplify.configure(outputs);
 
 function App() {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    const fetchAuthStatus = async () => {
+      try {const { username, userId, signInDetails } = await getCurrentUser();
+      console.log(`The username: ${username}`);
+      console.log(`The userId: ${userId}`);
+      console.log(`The signInDetails: ${signInDetails}`);
+      setIsAuthenticated(true)
+    }
+    catch (err) {
+      await signInWithRedirect({
+      provider: {
+        custom: 'IAMIdC'
+      }
+    });
+    }
+    }
+  
+    fetchAuthStatus()
+  
+   }, [])
 
   // useEffect(() => {
   //   client.models.Todo.observeQuery().subscribe({
@@ -17,20 +40,36 @@ function App() {
   //   });
   // }, []);
 
+  // return (
+  //   // <Authenticator>
+  //     // {({ signOut, user }) => (
+  //       <main>
+  //         {/* <h1>Hello {user?.username}</h1>
+  //         <button onClick={signOut}>Sign out</button> */}
+  //         {/* StorageBrowser Component */}
+  //         <h2>Your Files</h2>
+  //         <StorageBrowser />
+  //       </main>
+  //     // )}
+  //   // </Authenticator>
+  // );
+  const logout = async () => {
+    await signOut();
+  }
+  if (isAuthenticated) {
+    return (
+      <main>
+        {/* <h1>Hello {user?.username}</h1>*/}
+          <button onClick={logout}>Sign out</button> 
+        {/* StorageBrowser Component */}
+        <h2>Your Files</h2>
+        <StorageBrowser />
+      </main>
+    );
+  }
+  return <p>loading..</p>;
 
-  return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user?.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-          {/* StorageBrowser Component */}
-          <h2>Your Files</h2>
-          <StorageBrowser />
-        </main>
-      )}
-    </Authenticator>
-  );
+
 };
 
 

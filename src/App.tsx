ae87fe0dd985d81f1bf6a50467de671781f42b54
Vadicherla <vadicak1@novartis.net>
2025-03-27 14:55:46 +0530

    endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // import { useEffect, useState } from "react";
-import { Authenticator } from '@aws-amplify/ui-react';
+// import { Authenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import outputs from '../amplify_outputs.json';
 import '@aws-amplify/ui-react/styles.css';
